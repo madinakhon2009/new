189841b809cdad0a57424c098d78c6db52e19dcd
madina2009xon <mahkamboyevamadina2@gmail.com>
2024-10-14 17:20:44 +0300

    nw

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+alert("Olma: 15000 so`m");
+let olma =Number(prompt("Necha kg Olma kerak?"));
+
+alert("Nok: 20000 so`m");
+let nok =Number(prompt("Necha kg Nok kerak?"));
+
+alert("Shaftoli: 17000 so`m");
+let shaftoli=Number(prompt("Necha kg shaftoli kerak?"));
+
+alert("Uzum: 10000 so`m");
+let uzum=Number(prompt("Necha kg Uzum kerak?"));
+
+alert("Qulupnay:25000 so`m");
+let qulupnay=Number(prompt("Necha kg qulupnay kerak?"));
+
+
+alert(`Jami mahsulotlar: Olma ${olma * 15000} som , Nok ${nok * 20000}som , Shaftoli ${shaftoli * 17000} som, Uzum ${uzum * 10000} som, Qulupnay ${qulupnay * 25000} som `);
+
+alert(`Umumiy summa:${olma * 15000 + nok *  20000 + shaftoli * 17000 + uzum * 10000 + qulupnay * 25000} som`);
